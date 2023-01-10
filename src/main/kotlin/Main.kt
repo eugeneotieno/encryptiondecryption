0f318a3d49174efb65672f9a7ce1430d3c18bba9
Eugene Otieno <otienoeo@gmail.com>
2023-01-10 11:54:58 +0300

    Get arguments from command line

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,109 @@
-fun main() {
-    val target = readln()
-    val str = readln()
-    val shift = readln().toInt()
-    encryptDecryptUnicode(str, shift, target)
+import java.io.File
+import kotlin.math.abs
+
+fun main(args: Array<String>) {
+    var alg = "shift"
+    var target = "enc"
+    var shift = 0
+    var str = ""
+    var inFile = ""
+    var outFile = ""
+    var error = false
+    var errorMsg = ""
+
+    repeat(args.size) { index->
+        if (args[index] == "-alg")  {
+            if (args[index + 1] in arrayOf("-mode", "-key", "-data", "-in", "-out")) {
+                error = true
+                errorMsg = "Error: -alg not specified correctly!"
+            } else {
+                alg = args[index + 1]
+            }
+        }
+        if (args[index] == "-mode")  {
+            if (args[index + 1] in arrayOf("-alg", "-key", "-data", "-in", "-out")) {
+                error = true
+                errorMsg = "Error: -mode not specified correctly!"
+            } else {
+                target = args[index + 1]
+            }
+        }
+        if (args[index] == "-key") {
+            if (args[index + 1] in arrayOf("-alg", "-mode", "-data", "-in", "-out")) {
+                error = true
+                errorMsg = "Error: -key not specified correctly!"
+            } else {
+                shift = args[index + 1].toInt()
+            }
+        }
+        if (args[index] == "-data") {
+            if (args[index + 1] in arrayOf("-alg", "-mode", "-key", "-in", "-out")) {
+                error = true
+                errorMsg = "Error: -data not specified correctly!"
+            } else {
+                str = args[index + 1]
+            }
+        }
+        if (args[index] == "-in") {
+            if (args[index + 1] in arrayOf("-alg", "-mode", "-key", "-data", "-out")) {
+                error = true
+                errorMsg = "Error: -in not specified correctly!"
+            } else {
+                inFile = args[index + 1]
+            }
+        }
+        if (args[index] == "-out") {
+            if (args[index + 1] in arrayOf("-alg", "-mode", "-key", "-data", "-in")) {
+                error = true
+                errorMsg = "Error: -out not specified correctly!"
+            } else {
+                outFile = args[index + 1]
+            }
+        }
+    }
+
+    if (error) {
+        println("Error: $errorMsg")
+    } else {
+        if (str == "" && inFile == "") {
+            str = ""
+            showOutput(outFile, str, shift, target, alg)
+        } else {
+            if (str.isBlank() && inFile.isNotBlank()) {
+                if (File(inFile).exists()) {
+                    str = File(inFile).readText()
+                    showOutput(outFile, str, shift, target, alg)
+                } else {
+                    println("Error: Input file does not exist!")
+                }
+            } else {
+                showOutput(outFile, str, shift, target, alg)
+            }
+        }
+    }
 }
 
-fun encryptDecryptUnicode(str: String, shift: Int, target: String) {
+fun showOutput(outFile: String, str: String, shift: Int, target: String, alg: String) {
+    if (outFile == "") {
+        println(encryptDecryptUnicode(str, shift, target, alg))
+    } else {
+        File(outFile).writeText(encryptDecryptUnicode(str, shift, target, alg))
+    }
+}
+
+fun encryptDecryptUnicode(str: String, shift: Int, target: String, alg: String): String {
     val strArray = str.toCharArray()
-    repeat(strArray.size) { index ->
-        strArray[index] = getShiftedLetterUnicode(str[index], shift, target)
+
+    if (alg == "shift") {
+        repeat(strArray.size) { index ->
+            if (str[index].isLetter()) {
+                strArray[index] = getShiftedLetter(str[index], shift, target)
+            }
+        }
+    } else {
+        repeat(strArray.size) { index ->
+            strArray[index] = getShiftedLetterUnicode(str[index], shift, target)
+        }
     }
 
     var newStr = ""
@@ -16,7 +111,7 @@ fun encryptDecryptUnicode(str: String, shift: Int, target: String) {
         newStr += strArray[it].toString()
     }
 
-    println(newStr)
+    return newStr
 }
 
 fun getShiftedLetterUnicode(letter: Char, shift: Int, target: String): Char {
@@ -36,13 +131,30 @@ fun getShiftedLetterUnicode(letter: Char, shift: Int, target: String): Char {
     }
 }
 
-fun getShiftedLetter(letter: Char, shift: Int): Char {
-    val alphabet = "abcdefghijklmnopqrstuvwxyz"
+fun getShiftedLetter(letter: Char, shift: Int, target: String): Char {
+    when(target) {
+        "enc" -> {
+            val alphabet = if (letter.isLowerCase()) {
+                "abcdefghijklmnopqrstuvwxyz"
+            } else {
+                "abcdefghijklmnopqrstuvwxyz".uppercase()
+            }
+            var newPosition = alphabet.indexOf(letter) + shift
+            if (newPosition > (alphabet.length - 1)) newPosition = (newPosition - alphabet.length)
+            return alphabet.elementAt(newPosition)
+        }
 
-    var newPosition = alphabet.indexOf(letter) + shift
-    if (newPosition > (alphabet.length - 1)) newPosition = (newPosition - alphabet.length)
-
-    return alphabet.elementAt(newPosition)
+        else -> {
+            val alphabet = if (letter.isLowerCase()) {
+                "abcdefghijklmnopqrstuvwxyz"
+            } else {
+                "abcdefghijklmnopqrstuvwxyz".uppercase()
+            }
+            var newPosition = alphabet.indexOf(letter) - shift
+            if (newPosition < 0) newPosition = (alphabet.length - abs(newPosition))
+            return alphabet.elementAt(newPosition)
+        }
+    }
 }
 
 fun getCorrespondingLetter(letter: Char): Char {
